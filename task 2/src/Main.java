b75a5ac290d38694d3fe6a9bee45f966263a2969
SneakyZoul <78617698+SneakyZoul@users.noreply.github.com>
2021-03-04 13:23:10 +0100

    op

--- task 2/src/Main.java
+++ task 2/src/Main.java
@@ -6,23 +6,35 @@ public class Main {
     static Scanner scan;
     private static String[][] text = new String[12][1];
 
-    public static void main(String[] args) throws FileNotFoundException {
+
+    public static void main(String[] args) throws FileNotFoundException
+    {
 
 
         printLongestSentence();
 
     }
 
-    private static void printLongestSentence() throws FileNotFoundException {
-
-        File file = new File("data.txt");
+    private static void printLongestSentence() throws FileNotFoundException
+    {
+            String longest = "";
+             String[] splitarry = new String[0];
+        File file = new File("src/data.txt");
         scan = new Scanner(file);
         int i = 0;
         while (scan.hasNextLine()) {
-            String[] splitarray = scan.nextLine().split("\\. ");
+            splitarry = scan.nextLine().split("\\. ");
+            longest = splitarry[0];
+            for (String sentence: splitarry)
+            {
+                if (longest.length() <= sentence.length())
+                {
+                    longest = sentence;
+                }
+            }
             i++;
 
         }
-        System.out.println(file);
+        System.out.println(longest);
     }
 }
