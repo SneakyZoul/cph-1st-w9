2db5dded7e8c526eff5a5404a20363bec3c627df
SneakyZoul <78617698+SneakyZoul@users.noreply.github.com>
2021-03-04 10:06:21 +0100

    opg

--- cph-1st-w9-opg1.a/src/Main.java
+++ cph-1st-w9-opg1.a/src/Main.java
@@ -25,20 +25,16 @@ public class Main {
 
     public static void printWordsWithDoubleConsunant() {
 //how dió yo make it print alle words with double consunat?
-        System.out.println("Enter word: ");
         Scanner scanner = new Scanner(System.in);
-        String word = scanner.nextLine();
+        String text = scanner.nextLine();
         int i = 0;
         boolean found = false;
-        for (i = 0; i < word.length() - 1; i++) {
-            if (word.charAt(i) == word.charAt(i + 1)) {
+        for (i = 0; i < text.length() - 1; i++) {
+            if (text.charAt(i) == text.charAt(i + 1)) {
                 found = true;
                 break;
             }
         }
-        if (found) {
-            System.out.println("word contain double letter");
-        } else
-            System.out.println("normal word found");
+
     }
 }
